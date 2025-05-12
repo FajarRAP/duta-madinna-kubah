f7fe5114936ed1a64a7773850ce933c0c0c392b6
Fajar Riansyah <fajary781@gmail.com>
2025-05-12 18:32:19 +0700

    feat: add transition to nav link

--- resources/views/components/navbar/nav-link.blade.php
+++ resources/views/components/navbar/nav-link.blade.php
@@ -1,5 +1,5 @@
 <li x-on:click="open=false">
-    <a {{ $attributes->merge(['class' => 'block py-2 px-3 text-gray-900 rounded-sm md:p-0']) }}>
+    <a {{ $attributes->merge(['class' => 'block py-2 px-3 rounded-sm md:p-0 transition duration-500']) }}>
         {{ $title }}
     </a>
 </li>
