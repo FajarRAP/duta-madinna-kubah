77e5f0109a40388c53e4f35c1a3583ee44b0bf7d
Fajar Riansyah <fajary781@gmail.com>
2025-05-12 21:28:56 +0700

    feat: add dashboard view

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<x-auth-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Dashboard
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
+                <p class="text-gray-900">
+                    Berhasil Masuk
+                </p>
+            </div>
+        </div>
+    </div>
+</x-auth-layout>
