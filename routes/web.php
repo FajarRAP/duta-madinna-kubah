cadafba15c1fd2204604df18e71cfd2e1a312178
Fajar Riansyah <fajary781@gmail.com>
2025-05-15 02:10:48 +0700

    feat: refactor routes to use controller methods for information management

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\InformationController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', HomeController::class)
+    ->name('home');
 
 Route::get('/dashboard', fn() => view('dashboard'))
     ->middleware('auth')->name('dashboard');
-Route::get('/dashboard/information', fn() => view('information'))
+Route::get('/dashboard/information', [InformationController::class, 'index'])
     ->middleware('auth')->name('information');
+Route::post('dashboard/information', [InformationController::class, 'store'])
+    ->middleware('auth')->name('information.store');
+Route::delete('dashboard/information/{information}', [InformationController::class, 'destroy'])
+    ->middleware('auth')->name('information.destroy');
 Route::get('/dashboard/portfolio', fn() => view('portfolio'))
     ->middleware('auth')->name('portfolio');
+
 
 Route::get('/auth/login', fn() => view('auth.login'))
     ->name('login');
