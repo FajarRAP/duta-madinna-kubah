a1b8d83aa398dc876593f01acbc788a976921b04
Fajar Riansyah <fajary781@gmail.com>
2025-05-23 22:41:55 +0700

    refactor (index): adjust template

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
     public function __invoke(Request $request)
     {
         return view('index', [
-            'information' => Information::all(),
+            'informations' => Information::all(),
         ]);
     }
 }
