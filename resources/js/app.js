39eddab954ae4164255ccecb7cfa1ee72ccd8975
Fajar Riansyah <fajary781@gmail.com>
2025-05-28 14:36:56 +0700

    chore: remove jquery and slick carousel

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,51 +2,7 @@ import "./bootstrap";
 import.meta.glob(["../images/**", "../fonts/**"]);
 import Alpine from "alpinejs";
 import { collapse } from "@alpinejs/collapse";
-import $ from "jquery";
 
 window.Alpine = Alpine;
 Alpine.plugin(collapse);
 Alpine.start();
-
-$(function () {
-    $('[href^="#"]').on("click", function () {
-        const id = $(this).attr("href");
-        $("html, body").animate({ scrollTop: $(id).offset().top - 100 }, 500);
-    });
-
-    const sections = $("section");
-    const navlinks = $("nav ul li a");
-
-    $(this).on("scroll", function () {
-        let currentSection = "home";
-
-        sections.each(function () {
-            const sectionTop = $(this).offset().top;
-            const sectionHeight = $(this).outerHeight();
-            if (
-                pageYOffset >= sectionTop - 180 &&
-                pageYOffset < sectionTop + sectionHeight
-            ) {
-                currentSection = $(this).attr("id");
-            }
-        });
-
-        if (
-            $(window).scrollTop() + $(window).height() >=
-            $(document).height() - 10
-        ) {
-            currentSection = "contact";
-        }
-
-        navlinks.each(function () {
-            $(this).removeClass("text-blue-600").addClass("text-gray-900");
-            if ($(this).attr("href") === "#" + currentSection) {
-                $(this).addClass("text-blue-600").removeClass("text-gray-900");
-            }
-        });
-    });
-
-    $("#scrollToTop").on("click", function () {
-        $("html, body").animate({ scrollTop: 0 }, 500);
-    });
-});
