b5eb0a6b925255ddae6bec4a829d345752d2208a
Fajar Riansyah <fajary781@gmail.com>
2025-05-30 11:34:46 +0700

    refactor (promo item): change name to data card item

---
+++ resources/views/components/data-card.blade.php
@@ -0,0 +1,12 @@
+@props(['model'])
+
+<div class="data-card-item">
+    <div class="team-item bg-light rounded overflow-hidden">
+        <div class="team-img position-relative overflow-hidden">
+            <img class="img-fluid data-card-image" src="{{ asset("storage/{$model->image_path}") }}" alt="">
+        </div>
+        <div class="text-center py-4">
+            <h4 class="text-primary">{{ $model->title }}</h4>
+        </div>
+    </div>
+</div>
